e04f088eab03f8f41bc68812eda616348cc50048
S-Kill <frontendmonster@gmail.com>
2017-08-12 17:00:51 +0430

    fix about message

--- app/client/components/About/About.jsx
+++ app/client/components/About/About.jsx
@@ -5,7 +5,7 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">Hello from <span>React + Mongo + Express + Node + Webpack + HMRE3</span></h1>
+        <h1 className="title">Hello from <span>React + Mongo + Express + Node + Webpack + HMR + ES7 + ESLint </span></h1>
       </div>
     );
   }
