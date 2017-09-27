2ef4fcd0372d5411a2d93d1e7576e1e3a2e7564f
skill <frontendmonster@gmail.com>
2017-09-27 03:33:29 +0330

    Add redux-logger

--- app/client/redux/store/configure.js
+++ app/client/redux/store/configure.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../states';
 
 const configureStore = (initialState = {}) => {
-  return createStore(rootReducer, initialState);
+  const logger = createLogger();
+  return createStore(rootReducer, initialState, applyMiddleware(logger));
 };
 
 export default configureStore;
