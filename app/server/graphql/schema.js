40a23399c745626d9c7a7814ea3839110ed510f5
skill <frontendmonster@gmail.com>
2018-01-11 22:53:49 +0330

    Remove grpahql-tools

--- app/server/graphql/schema.js
+++ app/server/graphql/schema.js
@@ -1,9 +1,5 @@
-import path from 'path';
 import { makeExecutableSchema } from 'graphql-tools';
-import { importSchema } from 'graphql-import';
 import { resolvers } from './resolvers';
-
-const rootSchema = path.join(__dirname, 'schema.graphql');
-const typeDefs = importSchema(rootSchema);
+import typeDefs from './schema.graphql';
 
 export default makeExecutableSchema({ typeDefs, resolvers });
