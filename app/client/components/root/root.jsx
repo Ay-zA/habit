b5c0b30fecdb14cd47a34106a7543af33030f5c5
skill <frontendmonster@gmail.com>
2018-01-16 00:32:53 +0330

    Add graphql-eslint-plugin

--- app/client/components/root/root.jsx
+++ app/client/components/root/root.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
 import store from '-/redux/store';
 import App from '../App';
 
+const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 const Root = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>
 );
 
 export default Root;
