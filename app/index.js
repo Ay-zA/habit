afd22c050dfa99c18c923734ec5b9865e6aee1f0
skill <frontendmonster@gmail.com>
2018-04-28 16:09:09 +0430

    Extract clear console functionality

--- app/index.js
+++ app/index.js
@@ -1,11 +1,10 @@
-import clear from 'clear-it';
 import config from '<configs>';
 import logger from '<utils>/logger';
+import clear from '<utils>/clear-console';
 import { startServer } from './server';
 
 process.on('unhandledRejection', (err) => {
   logger.error(err.stack);
 });
 
-clear();
 startServer(config);
