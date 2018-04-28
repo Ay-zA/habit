f44a30d2f2b8255fc8cdbfd26208f513374b67d8
skill <frontendmonster@gmail.com>
2018-04-29 00:38:40 +0430

    Add email and password validators

--- app/server/models/user/user.schema.js
+++ app/server/models/user/user.schema.js
@@ -1,3 +1,5 @@
+import { isEmail } from 'validator';
+
 export default {
   email: {
     type: String,
@@ -5,14 +7,22 @@ export default {
     trim: true,
     required: [true, 'Email is required!'],
     lowercase: true,
+    validate: {
+      validator: isEmail,
+      message: '{VALUE} is not a valid email!',
+    },
   },
+
   name: {
     type: String,
     trim: true,
+    maxLength: [256, 'password must be of maximum 256 characters length!'],
   },
+
   password: {
     type: String,
     required: [true, 'Password is required!'],
-    minLength: [6, 'Password need to be longer!'],
+    minLength: [8, 'password must be of minimum 8 characters length!'],
+    maxLength: [256, 'password must be of maximum 256 characters length!'],
   },
 };
